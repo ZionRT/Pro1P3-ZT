c686e98f0750fd9d610aa6219258184aa28bbc5b
ZionRT <101285907+ZionRT@users.noreply.github.com>
2022-09-26 12:41:59 -0700

    Camera follows vehichle, full player control over vehichle (inputs move forward, backward, rotate, etc.) Added more obstacles, Added script to camera, changed game view tint to a yellow-green-ish color.

--- Create with Code/Prototype 1/Assets/Scripts/PlayerControl.cs
+++ Create with Code/Prototype 1/Assets/Scripts/PlayerControl.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    private float speed = 20.0f;
+    private float turnspeed = 90.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +17,11 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Move vehichle forward
-        transform.Translate(0,0,1);
+        horizontalInput = Input.GetAxis ("Horizontal");
+        forwardInput = Input.GetAxis ("Vertical");
+        //Move vehichle forward and backward on input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates direction vehichle is facing on input
+        transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime );
     }
 }
